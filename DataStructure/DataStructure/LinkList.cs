b1965408892ffc68e8cf87e17d328aafb0a5ebcc
Hallel <halleli100@gmail.com>
2019-11-12 12:20:01 +0200

    insert node

---
+++ DataStructure/DataStructure/LinkList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure 
+{
+
+    class LinkList<T> : LinkListInterface
+    {
+        private Node<T> head;
+
+        public LinkList(){
+            head = null;
+        }
+        public void AddFirst(T t)
+        {
+            Node<T> tmp = new Node<T>(t);
+            tmp.next = head;
+            head = tmp;
+        }
+    }
+}
---
+++ DataStructure/DataStructure/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    class Node<T>
+    {
+        public T data { get; set; }
+        public Node<T> next { get; set; }
+
+        public Node(T t)
+        {
+            next = null;
+            data = t;
+        }
+    }
+}
